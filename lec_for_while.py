f9ab3c7ba67e8de9fae309320730eb27d019f72d
Katsur0V1rus <radzhiv123321@gmail.com>
2024-10-05 09:57:05 +0000

    lec2

---
+++ lec_for_while.py
@@ -0,0 +1,27 @@
+for i in 1, 3, 4:
+    print(i**2, end=' ')
+print()
+for i in 1, 3, 4:
+    print(i**2, end='\n')
+for i in 1, 3, 4: 
+    print()
+    print(i, i**2, sep=' - ')
+
+a = [1, 5, 7, 11]
+for i in a: 
+    print()
+    print(f'{i}**3 = {i**3}')
+
+
+
+
+
+
+
+
+
+
+i = 5
+while i < 15: 
+    print('i: ', i)
+    i +=2
